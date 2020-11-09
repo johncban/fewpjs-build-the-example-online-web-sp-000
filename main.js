c863d61f04df54c8b8f5c51eca65a825ee62a8e5
Juan Carlo Banayo <johncban@gmail.com>
2020-11-09 00:52:01 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -8,6 +8,26 @@ let glyphStates = {
   "♥": "♡"
 };
 
+let colorStates = {
+  "red" : "",
+  "": "red"
+};
+
+let articleHearts = document.querySelectorAll(".like");
+
+
+function likeCallback(e) {
+  let heart = e.target;
+
+  mimicServerCall("bogusUrl")
+  .then(function(serverMessage) {
+    heart.innerText = glyphStates[heart.innerText];
+    heart.style.color = colorStates[heart.style.color];
+  })
+  .catch(function(error) {
+    document.getElementById("modal").className = "";
+  });
+}
 
 
 //------------------------------------------------------------------------------
